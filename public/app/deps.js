1f509b350be6eea949f0fb02475aea72de42dc51
Steven Degutis <sbdegutis@gmail.com>
2019-06-26 13:59:12 -0500

    Verified that deps can be versioned in a single central file!

---
+++ public/app/deps.js
@@ -0,0 +1,2 @@
+export {default as ReactDOM} from '/web_modules/react-dom@16.8.6.js';
+export {default as React} from '/web_modules/react@16.8.6.js';
--- public/app/util.js
+++ public/app/util.js
@@ -1,5 +1,6 @@
 // import { html } from '/web_modules/htm/react.js';
-import React from '/web_modules/react@16.8.6.js';
+import {ReactDOM} from '/app/deps.js';
+import {React} from '/app/deps.js';
 import { add1 } from '/app/util.js';
 import { ExternalLink } from "./Links.js";
 import styled from "/web_modules/styled-components.js";
